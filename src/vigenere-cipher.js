d88e8be9ff004eff8b83365499a683c92955dd9f
Semyon <cemen12532@gmail.com>
2023-04-18 02:16:16 +0500

    j

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const messageLength = message.length;
+    let result = '';
+
+    for (let i = 0, j = 0; i < messageLength; i++) {
+      const currentCharCode = message.charCodeAt(i);
+      if (currentCharCode < 65 || currentCharCode > 90) {
+        // Non-alphabetic character
+        result += message[i];
+        continue;
+      }
+
+      const keyCharCode = key.charCodeAt(j % key.length) - 65;
+      const encryptedCharCode = ((currentCharCode - 65 + keyCharCode) % 26) + 65;
+      result += String.fromCharCode(encryptedCharCode);
+      j++;
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    const encryptedMessageLength = encryptedMessage.length;
+    let result = '';
+
+    for (let i = 0, j = 0; i < encryptedMessageLength; i++) {
+      const currentCharCode = encryptedMessage.charCodeAt(i);
+      if (currentCharCode < 65 || currentCharCode > 90) {
+        // Non-alphabetic character
+        result += encryptedMessage[i];
+        continue;
+      }
+
+      const keyCharCode = key.charCodeAt(j % key.length) - 65;
+      const decryptedCharCode = ((currentCharCode - 65 - keyCharCode + 26) % 26) + 65;
+      result += String.fromCharCode(decryptedCharCode);
+      j++;
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
